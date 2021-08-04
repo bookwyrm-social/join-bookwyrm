1d5f89f1a2daa3c68b2c7f725297c45e1f2f10a5
Mouse Reeve <mousereeve@riseup.net>
2021-08-04 15:37:08 -0700

    Generate site from templates

---
+++ generate.py
@@ -0,0 +1,17 @@
+""" generate html files """
+from jinja2 import Environment, FileSystemLoader
+
+paths = [
+    "index.html",
+    "instances/index.html"
+]
+
+env = Environment(
+    loader=FileSystemLoader("templates/")
+)
+for path in paths:
+    template_string = open(f"templates/{path}", 'r').read()
+    template = env.from_string(template_string)
+
+    with open(f"site/{path}", "w") as render_file:
+        render_file.write(template.render())
