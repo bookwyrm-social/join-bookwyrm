9dae1d846fe9dfeb5451be424e18e980d12da05f
Mouse Reeve <mousereeve@riseup.net>
2022-12-05 09:26:10 -0800

    Ranks instances based on users, version, and open status

--- generate.py
+++ generate.py
@@ -14,6 +14,7 @@ env = Environment(loader=FileSystemLoader("templates/"), extensions=["jinja2.ext
 env.install_gettext_translations(i18n)
 
 
+# pylint: disable=too-many-locals
 def load_instances():
     """update the list of instances"""
     with open("instances.json", "r", encoding="utf-8") as list_file:
@@ -21,17 +22,22 @@ def load_instances():
 
     print("  Fetching instance statistics:")
     instance_data = []
+    release = requests.get(settings.RELEASE_API, timeout=settings.TIMEOUT)
+    current_version = version.parse(release.json().get("tag_name", None))
     for instance_url in instance_urls:
         print(f"  - Fetching: {instance_url}")
         try:
             response = requests.get(f"{instance_url}api/v1/instance", timeout=settings.TIMEOUT)
             data = response.json()
             version_number = data["version"]
-            if version.parse(version_number) < version.parse(settings.MINIMUM_VERSION):
+            parsed_version = version.parse(version_number)
+            if parsed_version < version.parse(settings.MINIMUM_VERSION):
                 raise Exception("Instance is out of date with version:", version_number)
+
             # pylint: disable=consider-using-f-string
             instance = {"path": instance_url}
             instance["users"] = "{:,}".format(data["stats"]["user_count"])
+            instance["users_int"] = int(data["stats"]["user_count"])
             instance["registration"] = (
                 "open"
                 if data["registrations"]
@@ -51,12 +57,30 @@ def load_instances():
             instance["logo"] = data["thumbnail"]
             instance["name"] = data["title"]
             instance["version"] = data["version"]
+
+            # Determine a sort order
+            rank = 0
+            # reduce weight of user count
+            rank += instance["users_int"] ** 0.5
+            # distance between this version and current version (how up to date is it?)
+            rank -= (current_version.major - parsed_version.major) * 100 + \
+                    (current_version.minor - parsed_version.minor) * 10 + \
+                    (current_version.micro - parsed_version.micro)
+            # prioritize open instance
+            if instance["registration"] == "open":
+                rank += 10
+            elif instance["registration"] == "invite":
+                rank += 5
+            instance["rank"] = -1 * rank
+
         except Exception as err:  # pylint: disable=broad-except
             print(f"    ! {err}")
             print("    - Site could possibly be down. Please check it manually:")
             print(f"    - Site url: {instance_url}")
             instance = {"path": instance_url, "skip": True}
         instance_data.append(instance)
+
+    instance_data = sorted(instance_data, key=lambda i: i["rank"])
     return instance_data
 
 
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 """ settings! at the generator file """
 MINIMUM_VERSION = "0.3.0"
 TIMEOUT = 10  # seconds to try to access an instance
+RELEASE_API = "https://api.github.com/repos/bookwyrm-social/bookwyrm/releases/latest"
